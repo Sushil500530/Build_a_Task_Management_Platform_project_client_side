ccb75fd3c5054a25fe9be6933ef24b9168620ec9
Sushil97 <sushil500530@gmail.com>
2023-12-20 23:01:39 +0600

    add error page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Mainlayout from "../layouts/Mainlayout";
+import ErrorPage from "../pages/error/ErrorPage";
 
 const Router = createBrowserRouter([
     {
         path: '/',
+        errorElement: <ErrorPage />,
         element: <Mainlayout />
     }
 ])
