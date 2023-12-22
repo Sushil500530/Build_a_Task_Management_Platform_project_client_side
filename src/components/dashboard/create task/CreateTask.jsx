424c30b6367b9df335d85d8f338dd1f873a203d4
Sushil97 <sushil500530@gmail.com>
2023-12-22 16:49:21 +0600

    add create task store in database

--- src/components/dashboard/create task/CreateTask.jsx
+++ src/components/dashboard/create task/CreateTask.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import Footer from "../../../pages/footer/Footer";
 import { FaSpinner } from 'react-icons/fa';
+import { MdCreate } from "react-icons/md";
 import toast from "react-hot-toast";
 import { imageUpload } from "../../../api/imageUpload";
 import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const CreateTask = () => {
-    const{user} = useAuth();
-    console.log(user);
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
     const [loading, setLoading] = useState(false);
     const handleCreateTask = async (e) => {
         setLoading(true);
@@ -30,11 +33,25 @@ const CreateTask = () => {
                 description,
                 startDate,
                 endDate,
-                taskOwnerName:user?.displayName,
-                taskOwnerEmail:user?.email,
-                tastOwnerPhoto:user?.photoURL
+                taskOwnerName: user?.displayName,
+                taskOwnerEmail: user?.email,
+                tastOwnerPhoto: user?.photoURL
             }
             console.log(taskDoc);
+            axiosSecure.post('/task-products', taskDoc)
+            .then(res => {
+                console.log(res.data);
+                if(res.data?.insertedId){
+                    setLoading(false);
+                    Swal.fire({
+                        title: "Wow🎉✨!",
+                        text: "Task Added Successfully!",
+                        icon: "success"
+                      });
+                    form.reset();
+                }
+            })
+            .catch(error => toast.error(error.message))
         }
         catch (error) {
             setLoading(false);
@@ -44,67 +61,65 @@ const CreateTask = () => {
     }
     return (
         <div>
-            <h1 className="text-2xl font-bold text-center my-8">Create Your Favourite Task</h1>
-            <div>
-                <form onSubmit={handleCreateTask} className=" p-5 ">
-                    <div className="flex gap-10">
-                        <div className="w-full space-y-3">
-                            <div className="flex flex-col gap-2">
-                                <label className="text-xl font-medium">Task Name*</label>
-                                <input type="text" placeholder="Task Name....." name="name" className="input input-bordered input-primary w-full text-[18px]" required />
-                            </div>
-                            <div className="flex flex-col gap-2">
-                                <label className="text-xl font-medium">Your Task Thumbnail*</label>
-                                <input type="file" name='thumbnail' className="file-input file-input-bordered file-input-primary w-full" required />
-                            </div>
-                            <div className="flex flex-col gap-2">
-                                <label className="text-xl font-medium">Task Type*</label>
-                                <select name="type" className="select select-primary w-full text-[18px] " >
-                                    <option disabled selected>Select Your Task Type</option>
-                                    <option value='hard'>Hard</option>
-                                    <option value='medium'>Medium</option>
-                                    <option value='low'>Low</option>
-                                </select>
-                            </div>
-                            <div className="flex flex-col gap-2">
-                                <label className="text-xl font-medium">Task Type*</label>
-                                <select name="category" className="select select-primary w-full text-[18px] " >
-                                    <option disabled selected>Select Category</option>
-                                    <option value='developer'>Developer</option>
-                                    <option value='data scientist'>Data Scientist</option>
-                                    <option value='graphics designer'>Graphics Designer</option>
-                                    <option value='banker'>Banker</option>
-                                    <option value='corporator'>Corporator</option>
-                                    <option value='digital marketer'>Digital Marketer</option>
-                                    <option value='education'>Education</option>
-                                </select>
-                            </div>
-                            <div className="flex flex-col gap-2">
-                                <label className="text-xl font-medium">Task Details*</label>
-                                <textarea className="textarea textarea-primary text-[18px]" placeholder="Write Details..." name="details" required></textarea>
-                            </div>
+            <h1 className="text-2xl font-bold text-center my-8 flex items-center justify-center gap-3">Create Your Favourite Task <span><MdCreate /></span></h1>
+            <form onSubmit={handleCreateTask} className=" p-5 ">
+                <div className="flex gap-10 mb-8">
+                    <div className="w-full space-y-3">
+                        <div className="flex flex-col gap-2">
+                            <label className="text-xl font-medium">Task Name*</label>
+                            <input type="text" placeholder="Task Name....." name="name" className="input input-bordered input-primary w-full text-[18px]" required />
                         </div>
-                        <div className="w-full space-y-3">
-                            <div className="flex flex-col gap-2">
-                                <label className="text-xl font-medium">Start Date*</label>
-                                <input type="date" placeholder="Task Name....." name="start_date" className="input input-bordered input-primary w-full text-[18px]" required />
-                            </div>
-                            <div className="flex flex-col gap-2">
-                                <label className="text-xl font-medium">End Date*</label>
-                                <input type="date" placeholder="Task Name....." name="end_date" className="input input-bordered input-primary w-full text-[18px]" required />
-                            </div>
-                            <button className="btn bg-gradient-to-r from-blue-800 to-blue-950 w-full text-xl hover:text-green-500 text-white mt-5">
-                                {loading ? (
-                                    <span className='flex items-center justify-center gap-3'> <FaSpinner className='m-auto animate-spin' size={24} /> Processing....</span>
-                                ) : (
-                                    'Created Task'
-                                )}
-                            </button>
+                        <div className="flex flex-col gap-2">
+                            <label className="text-xl font-medium">Your Task Thumbnail*</label>
+                            <input type="file" name='thumbnail' className="file-input file-input-bordered file-input-primary w-full" required />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label className="text-xl font-medium">Task Category*</label>
+                            <select name="category" className="select select-primary w-full text-[18px] " >
+                                <option disabled selected>Select Category</option>
+                                <option value='developer'>Developer</option>
+                                <option value='data scientist'>Data Scientist</option>
+                                <option value='graphics designer'>Graphics Designer</option>
+                                <option value='banker'>Banker</option>
+                                <option value='corporator'>Corporator</option>
+                                <option value='digital marketer'>Digital Marketer</option>
+                                <option value='education'>Education</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label className="text-xl font-medium">Task Details*</label>
+                            <textarea className="textarea textarea-primary text-[18px]" placeholder="Write Details..." name="details" required></textarea>
                         </div>
                     </div>
+                    <div className="w-full space-y-3">
+                        <div className="flex flex-col gap-2">
+                            <label className="text-xl font-medium">Task Type*</label>
+                            <select name="type" className="select select-primary w-full text-[18px] " >
+                                <option disabled selected>Select Your Task Type</option>
+                                <option value='hard'>Hard</option>
+                                <option value='medium'>Medium</option>
+                                <option value='low'>Low</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label className="text-xl font-medium">Start Date*</label>
+                            <input type="date" placeholder="Task Name....." name="start_date" className="input input-bordered input-primary w-full text-[18px]" required />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label className="text-xl font-medium">End Date*</label>
+                            <input type="date" placeholder="Task Name....." name="end_date" className="input input-bordered input-primary w-full text-[18px]" required />
+                        </div>
+                        <button className="btn bg-gradient-to-r from-blue-800 to-blue-950 w-full text-xl hover:text-green-500 text-white mt-5">
+                            {loading ? (
+                                <span className='flex items-center justify-center gap-3'> <FaSpinner className='m-auto animate-spin' size={24} /> Processing....</span>
+                            ) : (
+                                'Created Task'
+                            )}
+                        </button>
+                    </div>
+                </div>
 
-                </form>
-            </div>
+            </form>
             <Footer />
         </div>
     );
---
+++ src/components/hooks/useAxiosSecure.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+    baseURL: "http://localhost:5000",
+    withCredentials: true,
+})
+const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const { logoutUser } = useAuth();
+    useEffect(() => {
+        axiosSecure?.interceptors?.response?.use(res => {
+           // console.log(res);
+            return res;
+        }, error => {
+           // console.log(error);
+            if (error?.response?.status === 401 || error?.response?.status === 403) {
+                toast.error('cannot find your token! log out the user');
+                return navigate('/login')
+                // logoutUser()
+                //     .then(() => {
+                //         toast.error('cannot find your token! log out the user');
+                //     })
+                //     .catch(error => console.error(error))
+            }
+        })
+    }, [navigate, logoutUser])
+    return axiosSecure;
+};
+
+export default useAxiosSecure;
